6dda5768c1901078352fcb569def49cf0498a551
uniqcle <uniqcle@yandex.ru>
2021-12-08 21:13:05 +0700

    validation on change

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -37,11 +37,26 @@ class LoginForm extends Component {
     //call the server...
   };
 
-  handleChange = ({ currentTarget: input }) => {
-    const account = { ...this.state.account };
+  validateProperty = ({ name, value }) => {
+    if (name === "username") {
+      if (value.trim() === "") return "Username is required.";
+    }
 
+    if (name === "password") {
+      if (value.trim() === "") return "Password is required.";
+    }
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
+    const account = { ...this.state.account };
     account[input.name] = input.value;
-    this.setState({ account });
+    this.setState({ account, errors });
   };
 
   render() {
