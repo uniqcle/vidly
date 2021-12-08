20402f1ca2548e58f8fbbd45bbe04631fb08471e
uniqcle <uniqcle@yandex.ru>
2021-12-08 16:27:30 +0700

    login form

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
 
 class LoginForm extends Component {
+    username = React.createRef();
+
+    state = {
+        account: {
+            username: '',
+            password: ''
+        }
+    }
+
+    // componentDidMount() {
+    //     this.username.current.focus();
+    // }
+
+    handleSubmit = e => {
+        e.preventDefault();
+
+        const username = this.username.current.value;
+        //call the server...
+    }
 
     render() {
         return (
             <div className="container">
                 <h1>Login</h1>
-                <form action="">
-                    <div className="form-group"><label htmlFor="username">Username</label><input id="username" type="text" className="form-control" /></div>
-                    <div className="form-group"><label htmlFor="password">Password</label><input id="password" type="text" className="form-control" /></div>
+                <form onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="username">Username</label>
+                        <input ref={this.username} autoFocus id="username" type="text" className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password</label>
+                        <input id="password" type="text" className="form-control" />
+                    </div>
                     <button className="btn btn-primary">Login</button>
                 </form>
             </div>
