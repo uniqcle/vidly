5bd3000d3a26d4cb11bc4ba51d6c92f0e82f2be8
uniqcle <uniqcle@yandex.ru>
2021-12-08 20:54:34 +0700

    displaying validation errors

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -30,7 +30,7 @@ class LoginForm extends Component {
     e.preventDefault();
 
     const errors = this.validate();
-    this.setState({ errors });
+    this.setState({ errors: errors || {} });
     if (errors) return;
 
     //const username = this.username.current.value;
@@ -45,7 +45,7 @@ class LoginForm extends Component {
   };
 
   render() {
-    const { account } = this.state;
+    const { account, errors } = this.state;
 
     return (
       <div className="container">
@@ -56,6 +56,7 @@ class LoginForm extends Component {
             value={account.username}
             label="Username"
             onChange={this.handleChange}
+            error={errors.username}
           />
 
           <Input
@@ -63,6 +64,7 @@ class LoginForm extends Component {
             value={account.password}
             label="Password"
             onChange={this.handleChange}
+            error={errors.password}
           />
 
           <button className="btn btn-primary">Login</button>
